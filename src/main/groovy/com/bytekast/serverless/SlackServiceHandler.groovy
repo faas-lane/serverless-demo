ee8d82fdd48cee1561b3063402c00d35cc9e993c
Rowell Belen <developer@bytekast.com>
2017-05-22 13:45:49 -0600

    Sample Slack Command Service

--- src/main/groovy/com/bytekast/serverless/SlackServiceHandler.groovy
+++ src/main/groovy/com/bytekast/serverless/SlackServiceHandler.groovy
@@ -1,50 +1,63 @@
 package com.bytekast.serverless
 
 import com.amazonaws.services.lambda.runtime.Context
-import com.bytekast.serverless.trait.ToJson
+import com.bytekast.serverless.service.RestaurantService
+import com.bytekast.serverless.trait.SlackTemplate
 import groovy.util.logging.Log4j
 import org.codehaus.groovy.runtime.StackTraceUtils
 
-
 @Log4j
-class SlackServiceHandler implements ToJson {
+class SlackServiceHandler implements SlackTemplate {
+
+  private final CliBuilder commandParser
+  private final StringWriter helpUsage
+
+  SlackServiceHandler() {
+    commandParser = new CliBuilder()
+    commandParser.c(longOpt: 'cuisines', args: 1, argName: 'cuisine names', 'comma separated cuisines names')
+    commandParser.r(longOpt: 'radius', args: 1, argName: 'miles', 'radius in miles')
+
+    helpUsage = new StringWriter()
+    commandParser.usage = '/lunch'
+    commandParser.writer = new PrintWriter(helpUsage)
+    commandParser.usage()
+  }
 
   Map handler(Map input, Context context) {
 
     try {
       log.info "Slack request input: ${toJson(input)}"
-      def payload = input?.body?.split('&')?.collectEntries {
-        param -> param.split('=').collect { URLDecoder.decode(it, "UTF-8") }
-      }
+      def payload = slackPayload(input)
       log.info "Transformed slack request payload: ${toJson(payload)}"
 
+      // Authenticate Request
       verifyRequest(payload?.token)
 
-      def cli = new CliBuilder()
-      def stringWriter = new StringWriter()
-      cli.usage = '/serverless'
-      cli.writer = new PrintWriter(stringWriter)
-      cli.c(longOpt: 'command', args: 1, argName: 'name', 'command name')
-      cli.usage()
-
-      // TODO
-      // def options = cli.parse(payload?.text?.split())
-
-      if (payload?.text?.split()?.first() == 'help') {
-        return [body: toJson([text: stringWriter.toString()])]
-      } else {
-        return [body: toJson([text: payload?.user_name])]
+      // Return Help Usage
+      if (!payload?.text || payload?.text?.trim()?.startsWith('help')) {
+        return [body: toJson([text: helpUsage.toString()])]
       }
+
+      // Parse command
+      def options = commandParser.parse(payload?.text?.split())
+
+      // Call Restaurant Service
+      final int BOULDER_CITY_ID = 11184
+      final int DENVER_CITY_ID = 305
+      def cuisines = options.c ? options.c.toString()?.trim() : ''
+      def radius = options.r ? options.r.toInteger() : 5
+      def restaurants = RestaurantService.instance.search(BOULDER_CITY_ID, cuisines, radius)
+
+      // Pick random restaurant
+      Collections.shuffle(restaurants)
+      def randomRestaurant = restaurants.first()
+      log.info randomRestaurant
+
+      return [body: toJson([text: randomRestaurant])]
+
     } catch (e) {
       log.warn e.message
       ['error': StackTraceUtils.extractRootCause(e).message]
-    }
-  }
-
-  void verifyRequest(String token) {
-    def slackCommandToken = System.getenv('SLACK_COMMAND_VERIFICATION_TOKEN')
-    if (!slackCommandToken?.trim() || token?.trim() != slackCommandToken) {
-      throw new Exception('Invalid or missing slack command token')
     }
   }
 }
